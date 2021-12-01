f81676dad3a5f06ee87f553b68b48d53b0ac72a2
Jake Albaugh <jake.albaugh@gmail.com>
2021-12-01 18:29:03 +0000

    random shapes and class based

--- app.js
+++ app.js
@@ -42,126 +42,73 @@ class RNG {
     };
   }
 }
-const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
-canvas.height = window.innerHeight * 2;
-canvas.width = window.innerWidth * 2;
 
-canvas.addEventListener("click", () => run(true));
-
-run();
-
-function run(forceSeed = false) {
-  const seedMatch = window.location.search.match(/s=(.+)/);
-  const seed = seedMatch && !forceSeed ? seedMatch[1] : Date.now().toString();
-  const searchParams = new URLSearchParams(window.location.search);
-  searchParams.set("s", seed);
-  const path = [
-    window.location.protocol,
-    "//",
-    window.location.host,
-    window.location.pathname,
-    "?",
-    searchParams.toString(),
-  ].join("");
-  window.history.replaceState({ path }, "", path);
-
-  const rng = new RNG(seed);
-
-  function rotate(cx, cy, x, y, angle) {
-    const radians = (Math.PI / 180) * angle;
-    const cos = Math.cos(radians);
-    const sin = Math.sin(radians);
-    const nx = cos * (x - cx) + sin * (y - cy) + cx;
-    const ny = cos * (y - cy) - sin * (x - cx) + cy;
-    return [nx, ny];
+class Polytension {
+  constructor() {
+    this.reset();
   }
 
-  const generations = Math.floor(rng.random() * 2) + 4;
-  const jitter = rng.random() * 0.02 + 0.001;
-  const shapes = {};
-
-  let id = 0;
-  const points = {};
-  const pointIds = [];
-  class Point {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.id = id++;
-      pointIds.push(this.id);
-      points[this.id] = this;
+  reset() {
+    delete this.points;
+    delete this.pointIds;
+    delete this.rng;
+    delete this.shapes;
+    this.points = {};
+    this.pointIds = [];
+    this.id = 0;
+    this.shapes = [];
+    if (this.animationFrame) {
+      cancelAnimationFrame(this.animationFrame);
     }
   }
 
-  const angle = rng.random() * 360;
-  traverse(
-    new Point(...rotate(0.5, 0.5, 0.5, 0.1, angle)),
-    new Point(...rotate(0.5, 0.5, 0.9, 0.75, angle)),
-    new Point(...rotate(0.5, 0.5, 0.1, 0.75, angle))
-  );
+  createPoint(x, y) {
+    this.id++;
+    const point = { x, y, id: this.id };
+    this.pointIds.push(point.id);
+    this.points[point.id] = point;
+    return point;
+  }
 
-  const items = [];
-  Object.values(shapes).forEach((generation, i) => {
-    generation.forEach((item) => {
-      items.push({ ...item, generation: i });
-    });
-  });
+  getAndSaveSeed(forceSeed) {
+    const seedMatch = window.location.search.match(/s=(.+)/);
+    const seed = seedMatch && !forceSeed ? seedMatch[1] : Date.now().toString();
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("s", seed);
+    const { protocol, host, pathname } = window.location;
+    const path = [
+      protocol,
+      "//",
+      host,
+      pathname,
+      "?",
+      searchParams.toString(),
+    ].join("");
+    window.history.replaceState({ path }, "", path);
+    return seed;
+  }
 
-  loop();
-
-  function loop() {
+  animationLoop() {
     context.fillStyle = "black";
     context.fillRect(0, 0, canvas.width, canvas.height);
-    items.forEach(draw);
-    pointIds.forEach((id) => morph(points[id]));
-    requestAnimationFrame(loop);
+    this.shapes.forEach(this.drawShape.bind(this));
+    this.pointIds.forEach(this.morphPoint.bind(this));
+    this.animationFrame = requestAnimationFrame(this.animationLoop.bind(this));
   }
 
-  function morph(item) {
+  morphPoint(pointId) {
+    const point = this.points[pointId];
     const clamp = (num) => Math.min(1, Math.max(0, num));
-    item.x = clamp(item.x * (rng.random() * jitter + (1 - jitter * 0.5)));
-    item.y = clamp(item.y * (rng.random() * jitter + (1 - jitter * 0.5)));
+    const morphInt = (int) =>
+      int * (this.rng.random() * this.jitter + (1 - this.jitter * 0.5));
+    point.x = clamp(morphInt(point.x));
+    point.y = clamp(morphInt(point.y));
   }
 
-  function traverse(A, B, C, generation = 0) {
-    shapes[generation] = shapes[generation] || [];
-    const hue = Math.round(rng.random() * 360);
-    shapes[generation].push({ A, B, C, hue });
-
-    const centerOrBisect = generation % 2 === 0 ? "center" : "bisect";
-    let newPoint;
-    if (centerOrBisect === "center") {
-      const randX = rng.random() * 0.4 - 0.2 + 1;
-      const randY = rng.random() * 0.4 - 0.2 + 1;
-      id++;
-      newPoint = new Point(
-        ((A.x + B.x + C.x) / 3) * randX,
-        ((A.y + B.y + C.y) / 3) * randY
-      );
-    } else {
-      const randX = rng.random() * 0.15 - 0.075 + 1;
-      const randY = rng.random() * 0.15 - 0.075 + 1;
-      newPoint = new Point(
-        ((A.x + B.x) / 2) * randX,
-        ((A.y + B.y) / 2) * randY
-      );
-    }
-    if (generation >= generations) {
-      return;
-    }
-
-    traverse(A, C, newPoint, generation + 1);
-    traverse(B, C, newPoint, generation + 1);
-    if (centerOrBisect === "center") {
-      traverse(A, B, newPoint, generation + 1);
-    }
-  }
-
-  function draw({ A, B, C, hue, generation }) {
+  drawShape({ A, B, C, hue, generation }) {
     const contextualize = (x, y) => [x * canvas.width, y * canvas.height];
     context.fillStyle = `hsla(${hue}, 100%, ${
-      (generation / generations) * 40 + 20
+      (generation / this.generations) * 40 + 20
     }%, 0.2)`;
     context.beginPath();
     context.moveTo(...contextualize(A.x, A.y));
@@ -170,4 +117,114 @@ function run(forceSeed = false) {
     context.closePath();
     context.fill();
   }
+
+  permuteForGenerations(A, B, C, generation = 0) {
+    const hue = Math.round(this.rng.random() * 360);
+    const shape = { A, B, C, hue, generation };
+    this.shapes.push(shape);
+
+    const centerOrBisect = generation % 2 === 0 ? "center" : "bisect";
+    let newPoint;
+    if (centerOrBisect === "center") {
+      const randX = this.rng.random() * 0.4 - 0.2 + 1;
+      const randY = this.rng.random() * 0.4 - 0.2 + 1;
+      newPoint = this.createPoint(
+        ((A.x + B.x + C.x) / 3) * randX,
+        ((A.y + B.y + C.y) / 3) * randY
+      );
+    } else {
+      const randX = this.rng.random() * 0.15 - 0.075 + 1;
+      const randY = this.rng.random() * 0.15 - 0.075 + 1;
+      newPoint = this.createPoint(
+        ((A.x + B.x) / 2) * randX,
+        ((A.y + B.y) / 2) * randY
+      );
+    }
+    if (generation >= this.generations) {
+      return;
+    }
+
+    this.permuteForGenerations(A, C, newPoint, generation + 1);
+    this.permuteForGenerations(B, C, newPoint, generation + 1);
+    if (centerOrBisect === "center") {
+      this.permuteForGenerations(A, B, newPoint, generation + 1);
+    }
+  }
+
+  run(forceSeed = false) {
+    this.reset();
+
+    this.rng = new RNG(this.getAndSaveSeed(forceSeed));
+
+    this.generations = Math.floor(this.rng.random() * 2) + 4;
+    this.jitter = this.rng.random() * 0.02 + 0.001;
+    this.angle = this.rng.random() * 360;
+
+    const shapes = this.randomShapes();
+    shapes.forEach((points) => this.permuteForGenerations(...points));
+
+    this.animationLoop();
+  }
+
+  randomShapes() {
+    const rand = this.rng.random();
+    if (rand <= 0.5) {
+      return [
+        [
+          this.createPoint(
+            ...this.rotateCoordinates(0.5, 0.5, 0.5, 0.1, this.angle)
+          ),
+          this.createPoint(
+            ...this.rotateCoordinates(0.5, 0.5, 0.9, 0.75, this.angle)
+          ),
+          this.createPoint(
+            ...this.rotateCoordinates(0.5, 0.5, 0.1, 0.75, this.angle)
+          ),
+        ],
+      ];
+    } else {
+      const pointA = this.createPoint(
+        ...this.rotateCoordinates(0.5, 0.5, 0.9, 0.5, this.angle)
+      );
+      const pointB = this.createPoint(
+        ...this.rotateCoordinates(0.5, 0.5, 0.1, 0.5, this.angle)
+      );
+      return [
+        [
+          pointA,
+          pointB,
+          this.createPoint(
+            ...this.rotateCoordinates(0.5, 0.5, 0.5, 0.1, this.angle)
+          ),
+        ],
+        [
+          pointA,
+          pointB,
+          this.createPoint(
+            ...this.rotateCoordinates(0.5, 0.5, 0.5, 0.9, this.angle)
+          ),
+        ],
+      ];
+    }
+  }
+
+  rotateCoordinates(cx, cy, x, y, angle) {
+    const radians = (Math.PI / 180) * angle;
+    const cos = Math.cos(radians);
+    const sin = Math.sin(radians);
+    const nx = cos * (x - cx) + sin * (y - cy) + cx;
+    const ny = cos * (y - cy) - sin * (x - cx) + cy;
+    return [nx, ny];
+  }
 }
+
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+canvas.height = window.innerHeight * 2;
+canvas.width = window.innerWidth * 2;
+
+const polytension = new Polytension();
+
+polytension.run();
+
+canvas.addEventListener("click", () => polytension.run(true));
